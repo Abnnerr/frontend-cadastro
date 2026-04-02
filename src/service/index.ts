3912fd573dd3a448815dd66c89062159f514f1c4
Abnnerr <jonnas.ab.00@gmail.com>
2026-04-02 12:07:41 -0300

    updates

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios, { type AxiosInstance } from "axios";
 
-export const AXIOS = axios.create({
+export const AXIOS: AxiosInstance = axios.create({
     baseURL: "http://localhost:3000"
 }) 
